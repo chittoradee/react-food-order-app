e064e0a30f31c994059b93876603a0271be7536c
Deepak Chittora <chittoradee@gmail.com>
2021-12-09 10:14:28 +0530

    cart item add and remove

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,39 @@
-import React, {useContext} from "react";
+import React, { useContext,useState,useEffect } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import CartContext from '../../store/cart-context';
-const HeaderCartButton = props => {
-    const cartCtx = useContext(CartContext);
+import CartContext from "../../store/cart-context";
+const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+	const cartCtx = useContext(CartContext);
+    const {items} = cartCtx;
+	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+		return (curNumber += item.amount);
+	}, 0);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber,item) => { return curNumber += item.amount},0);
-    return (
-        <button className={classes.button} onClick={props.onButtonClick}>
-            <span className={classes.icon}>
-                <CartIcon />
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>{numberOfCartItems}</span>
-        </button>
-    );
-}
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(()=>{
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+        const timer  = setTimeout(()=>{
+            setBtnIsHighlighted(false);
+        },300);
+        return ()=>{
+            clearTimeout(timer);
+        }
+    },[items]);
+
+	return (
+		<button className={btnClasses} onClick={props.onButtonClick}>
+			<span className={classes.icon}>
+				<CartIcon />
+			</span>
+			<span>Your Cart</span>
+			<span className={classes.badge}>{numberOfCartItems}</span>
+		</button>
+	);
+};
 
 export default HeaderCartButton;
